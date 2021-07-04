7690440762df053cf82b71eda28bbbeffda2e336
UgurCanGulec <gulecugurcan@gmail.com>
2021-07-04 16:46:30 +0300

    initialize commit

---
+++ eureka-server/src/main/java/com/j4v4developers/eureka_server/EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.j4v4developers.eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
